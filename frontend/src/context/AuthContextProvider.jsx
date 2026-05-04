0381977d338342d1358d784f6cf69e5d613d84fd
abubakarafzal0101 <abubakarafzal0101@gmail.com>
2026-05-04 16:24:16 +0500

    few changes

--- frontend/src/context/AuthContextProvider.jsx
+++ frontend/src/context/AuthContextProvider.jsx
@@ -4,11 +4,12 @@ import { useNavigate } from "react-router-dom";
 export const AuthContext = createContext();
 import { toast } from "react-hot-toast";
 import { UserContext } from "./UserContextProvider";
+import { PostContext } from "./PostContextProvider";
 const AuthContextProvider = ({ children }) => {
   const serverUrl = import.meta.env.VITE_SERVER_URL;
   const { getCurrentUser } = useContext(UserContext);
   const navigate = useNavigate();
-
+  const { getAllPosts } = useContext(PostContext);
   const handleSignup = async (formData) => {
     try {
       const response = await axios.post(
@@ -22,6 +23,7 @@ const AuthContextProvider = ({ children }) => {
       if (response.data.success) {
         toast.success(response.data.message);
         getCurrentUser();
+        getAllPosts();
         navigate("/login");
       }
     } catch (error) {
@@ -42,6 +44,7 @@ const AuthContextProvider = ({ children }) => {
       if (response.data.success) {
         toast.success(response.data.message);
         getCurrentUser();
+        getAllPosts();
         navigate("/");
       }
     } catch (error) {
@@ -58,6 +61,7 @@ const AuthContextProvider = ({ children }) => {
       if (response.data.success) {
         toast.success(response.data.message);
         getCurrentUser();
+        getAllPosts();
         navigate("/login");
       }
     } catch (error) {
